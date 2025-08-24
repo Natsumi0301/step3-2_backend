a3bb284d73d5c06c28cf16691cf03694f0d2f096
おかぴー <masaokada44@gmail.com>
2025-08-24 18:16:25 +0900

    no message

--- crud.py
+++ crud.py
@@ -43,8 +43,12 @@ def get_questions_from_db(db: Session) -> List[models.Question]:
             order_by=func.rand()
         ).label('row_num')
     ).subquery()
-    questions = db.query(subquery).filter(subquery.c.row_num == 1).all()
-    return questions
+    # questions = db.query(subquery).filter(subquery.c.row_num == 1).all()
+    # return questions
+    questions = db.query(subquery.c.question_id,
+    subquery.c.question_text,
+    subquery.c.category_id).filter(subquery.c.row_num == 1).all()
+    return [db.query(models.Question).filter(models.Question.question_id == q[0]).first() for q in questions]
 
 def get_random_recommendations_by_color_id(db: Session, color_id: int, limit: int) -> List[models.Recommendation]:
     """
